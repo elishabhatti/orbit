0ab807f53e4d61c90ab3bf43cb1ba24c8a51dbe6
elisha <elishajameel2007@gmail.com>
2026-05-12 11:03:46 +0500

    feat:used services in create workspace

--- src/app/api/workspace/create/route.ts
+++ src/app/api/workspace/create/route.ts
@@ -1,64 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
-
 import { connectDB } from "@/src/lib/db";
 import { getAuthUser } from "@/src/lib/auth";
-import { workspaceModel } from "@/src/models/workspace.model";
-
-interface CreateWorkspaceBody {
-  name: string;
-  description?: string;
-}
+import { WorkspaceService } from "@/src/services/workspace.services";
 
 export async function POST(req: NextRequest) {
-  try {
-    await connectDB();
+  await connectDB();
 
-    const user = await getAuthUser();
+  const user = await getAuthUser();
 
-    if (!user) {
-      return NextResponse.json(
-        { success: false, message: "Unauthorized" },
-        { status: 401 }
-      );
-    }
-
-    const body: CreateWorkspaceBody = await req.json();
-
-    if (!body.name) {
-      return NextResponse.json(
-        { success: false, message: "Workspace name required" },
-        { status: 400 }
-      );
-    }
-
-    const workspace = await workspaceModel.create({
-      name: body.name,
-      description: body.description,
-
-      owner: user._id,
-
-      members: [
-        {
-          user: user._id,
-          role: "owner",
-        },
-      ],
-    });
-
-    return NextResponse.json(
-      {
-        success: true,
-        workspace,
-      },
-      { status: 201 }
-    );
-  } catch (err: any) {
-    return NextResponse.json(
-      {
-        success: false,
-        message: err.message,
-      },
-      { status: 500 }
-    );
+  if (!user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
+
+  const body = await req.json();
+
+  const workspace = await WorkspaceService.createWorkspace(user._id, body);
+
+  return NextResponse.json({
+    success: true,
+    workspace,
+  });
 }
