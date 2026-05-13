de71724192b1b02e7a2aa67639ef5a88c8836377
elisha <elishajameel2007@gmail.com>
2026-05-13 10:44:34 +0500

    updated layout cz shortcut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      cz-shortcut-listen="true"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body cz-shortcut-listen="true" className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
